8af5ccba8ea085339cdbfef21ca6e401fbf7631b
lk785 <64523440+lk785@users.noreply.github.com>
2022-02-08 15:32:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
 const addbutton = document.querySelector(".addButton");
-const searchContainer = document.querySelector(".searchBox__container");
-const delButton = document.querySelectorAll(".delButton");
-let list = "";
-addbutton.addEventListener("click", function (e) {
-  console.log("clicked");
-  let inputVal = document.getElementById("sBox").value;
-  console.log(inputVal);
-  list = `
-  <div class="note__container" id="node__C">
-      <div class="notes">${inputVal}</div>
-      <button class="editButton">Edit</button>
-      <button class="delButton ">Delete</button>
-    </div>`;
-  searchContainer.insertAdjacentHTML("afterend", list);
-  document.getElementById("sBox").value = "";
-});
-for (let i = 0; i < delButton.length; i++) {
-  delButton[i].addEventListener("click", function () {
-    console.log("delete");
-    // let element = document.querySelector("#node__C");
-    // element.style.display = "none";
-    this.parentElement.remove();
+let input = document.querySelector(".searchBox");
+
+//function to add into the list
+const addList = function (e) {
+  const notComplete = document.querySelector(".not_Complete");
+  const completed = document.querySelector(".Completed");
+
+  const newLi = document.createElement("li");
+
+  const checkBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+
+  checkBtn.innerHTML = `<i class="fa fa-check"></i>`;
+  delBtn.innerHTML = `<i class="fa fa-trash"></i>`;
+
+  if (input.value === "") {
+    alert("Please enter value");
+  } else {
+    newLi.textContent = input.value;
+    input.value = "";
+    notComplete.appendChild(newLi);
+    newLi.appendChild(checkBtn);
+    newLi.appendChild(delBtn);
+  }
+
+  checkBtn.addEventListener("click", function (e) {
+    const parent = this.parentNode;
+    parent.remove();
+    completed.appendChild(parent);
+    checkBtn.style.display = "none";
   });
-}
+
+  delBtn.addEventListener("click", function (e) {
+    const parent = this.parentNode;
+    parent.remove();
+  });
+};
+
+addbutton.addEventListener("click", addList);
 
 // for (var i = 0; i < del.length; i++) {
 //   del[i].addEventListener("click", deleteListElement);
 // }
+
+// console.log(inputVal);
+//   list = `
+// <ul class="note__container" id="node__C">
+//       <li>
+//         ${inputVal}
+//         <button class="editButton">Edit</button>
+//         <button class="delButton">Delete</button>
+//       </li>
+//     </ul>`;
+//   searchContainer.insertAdjacentHTML("afterend", list);
+//   document.getElementById("sBox").value = "";
